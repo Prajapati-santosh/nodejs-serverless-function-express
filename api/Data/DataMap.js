336ad2fa90e5642fd95cd03d994454f6ad4af483
Santosh Prajapati <101245775+Prajapati-santosh@users.noreply.github.com>
2024-09-03 10:48:23 +0000

    API commit

---
+++ api/Data/DataMap.js
@@ -0,0 +1,10 @@
+const products = [
+    { id: 1, productName: 'Laptop' },
+    { id: 2, productName: 'Smartphone' },
+    { id: 3, productName: 'Tablet' },
+    { id: 4, productName: 'Headphones' },
+    { id: 5, productName: 'Smartwatch' },
+    { id: 6, productName: 'Lapi'}
+];
+
+export default products;
---
+++ api/index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import data from "./Data/DataMap.js";
+const app=express();
+
+
+app.get("/",(req,res)=>{
+    res.send("hello");
+})
+
+app.get("/data/:key",(req,res)=>{
+    if(req.params.key==process.env.API_KEY){
+        const query = req.query.name;
+        if (!query) {
+            return res.json([]);
+        }
+        const results = data.filter(item =>
+            item.productName.toLowerCase().includes(query.toLowerCase())
+        );
+        res.json(results);
+    }
+   else{
+    res.send("Not authenticated");
+   }
+})
+
+app.listen(process.env.PORT,()=>{
+    console.log(`server running on ${process.env.PORT}`);
+    
+})
+
+export default app;
