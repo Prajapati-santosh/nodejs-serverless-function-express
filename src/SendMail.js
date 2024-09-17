d7ec7394e06f50945e79a8cb9789911591a12540
Santosh Prajapati <101245775+Prajapati-santosh@users.noreply.github.com>
2024-09-17 08:50:41 +0000

    custom-template-html

--- src/SendMail.js
+++ src/SendMail.js
@@ -1,6 +1,10 @@
 import nodemailer from "nodemailer";
 import generateOTP from "./generateOtp.js";
 
+import path from "path";
+import handlebars from "handlebars";
+import fs from "fs";
+
 const transporter=nodemailer.createTransport({
     host:"smtp.gmail.com",
     port: 465,
@@ -13,11 +17,20 @@ const transporter=nodemailer.createTransport({
 
 async function sendEmail(email){
     const otp=generateOTP();
+    const __dirname = path.resolve();
+    const filePath = path.join(__dirname, "./public/emailTemplate.html");
+    const source = fs.readFileSync(filePath, 'utf-8').toString();
+    const template = handlebars.compile(source);
+    const replacements = {
+        OTP_CODE: otp
+      };
+    const htmlToSend = template(replacements);
     await transporter.sendMail({
         from:"emailer.otp.generate@gmail.com",
         to:email,
         subject:"OTP for login/signup to e-commerce",
         text: `Here is you otp for email verification ${otp}`,
+        html:htmlToSend
     }).then(()=>{
         console.log("otp sent");
     }).catch((error)=>{
